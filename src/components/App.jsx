95bf7720202e72a1c66e791a34dfc6981b93d02c
Katherine Lewis <kale8432@colorado.edu>
2019-05-31 17:44:02 -0600

    Build out app with search bar style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Controls from '../containers/Controls';
+import CardContainer from '../containers/Controls';
 import '../styles/App.scss';
 
 export class App extends Component {
 	render() {
 		return (
-			<div>
+			<div className="app">
 				<h1>303 Farmacy</h1>
+				<i class="fas fa-search" />
+				<input type="text" className="searchBar" placeholder="Search..." name="search" />
+				<Controls />
+				<CardContainer />
 			</div>
 		);
 	}
